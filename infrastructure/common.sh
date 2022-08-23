1d1ac3ee742fe014851f3ba246deec98a6f1cc10
Aleksey Kuznetsov <alexei.kuznetsov@corp.mail.ru>
2022-08-23 20:43:15 +0300

    add one more script

--- infrastructure/common.sh
+++ infrastructure/common.sh
@@ -1,6 +1,12 @@
 run_command () {
 
 clear
+if [ ! -z "$2" ]; then
+    cd $2;
+fi
+echo ""
+echo $(pwd)
+
 echo "запуск команды"
 echo ""
 echo "     [1;36m$1"
@@ -8,11 +14,6 @@ echo "[0m"
 
 read
 echo "вывод команды"
-if [ ! -z "$2" ]; then
-    cd $2;
-fi
-
-echo $(pwd)
 eval $1
 
 }
